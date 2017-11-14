89f01d10b7bd50b5fa912ad24132c1dbe851e73d
clairew2018 <ywang8@brynmawr.edu>
2017-11-14 01:13:46 -0500

    added face detection from openCV

--- detectFace.py
+++ detectFace.py
@@ -1,6 +1,6 @@
 '''
   File name: detectFace.py
-  Author:
+  Author: 
   Date created:
 '''
 
@@ -10,8 +10,32 @@
     - Input img: the first frame of video
     - Output bbox: the four corners of bounding boxes for all detected faces
 '''
+import PIL
+from pylab import *
+import numpy as np
+import cv2
 
 def detectFace(img):
   #TODO: Your code here 
-  
+  # read image to array
+  cascPath = 'haarcascade_frontalface_default.xml'
+  faceCascade = cv2.CascadeClassifier(cascPath)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  # Detect faces in the image
+  faces = faceCascade.detectMultiScale(
+  	gray,
+  	scaleFactor=2,
+  	minNeighbors=5,
+  	minSize=(40, 40),
+  	flags = cv2.CASCADE_SCALE_IMAGE
+  	)
+  bbox = []
+  # Draw a rectangle around the face
+  for (x, y, w, h) in faces:
+  	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+  	print (x,y,x+w,y+h)
+  	bbox.append([x,y,x+w,y+h])
+  #bbox = np.array(bbox)
+  cv2.imshow("Faces found" ,img)
+  cv2.waitKey(0)
   return bbox
---
+++ test.py
@@ -0,0 +1,5 @@
+from detectFace import detectFace
+import cv2
+
+image = cv2.imread('test2.jpg')
+detectFace(image)
