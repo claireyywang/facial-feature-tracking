26757eb4f8acecae6ed64c1b21ae49cef9fd495a
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-11-15 12:59:08 -0500

    re-organize files

---
+++ python_function/basic_packages.py
@@ -0,0 +1,13 @@
+'''
+File clarification:
+    import basic library such as numpy and so on
+
+
+'''
+
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os, math, scipy.misc, pdb, collections, random
+
---
+++ python_function/optical_flow_functions.py
@@ -0,0 +1,7 @@
+'''
+  File clarification:
+  Include any helper function you want for this project such as the
+  video frame extraction, video generation, drawing bounding box and so on.
+'''
+
+from python_function.detectFace import detectFace
