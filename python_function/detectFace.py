9bde06dcb340ba58d7e3168349c4b624f3e50c2d
clairew2018 <ywang8@brynmawr.edu>
2017-11-15 16:46:20 -0500

    added feature points - need adjustments

--- python_function/detectFace.py
+++ python_function/detectFace.py
@@ -11,6 +11,7 @@
     - Output bbox: the four corners of bounding boxes for all detected faces
 '''
 import cv2
+import numpy as np
 
 def detectFace(img):
   #TODO: Your code here 
@@ -25,18 +26,29 @@ def detectFace(img):
   	#scale factor decides the accuracy of detection
   	scaleFactor=1.3,
   	minNeighbors=5,
-  	minSize=(30, 30),
+  	minSize=(30, 40),
   	flags = cv2.CASCADE_SCALE_IMAGE
   	)
   bbox = []
   # Draw a rectangle around the face
   for (x, y, w, h) in faces:
   	cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-  	print ([[x,y], [x+w,y], [x,y+h],[x+w,y+h]])
-  	bbox.append([[x,y], [x+w,y], [x,y+h],[x+w,y+h]])
-  #bbox = np.array(bbox)
+  	print ([(x,y),(x+w,y),(x,y+h),(x+w,y+h)])
+  	bbox.append([(x,y),(x+w,y),(x,y+h),(x+w,y+h)])
+  bbox = np.array(bbox)
   #cv2.imshow("Faces found" ,img)
   cv2.imwrite("result.jpg", img)
   #click on any key to terminate display 
   #cv2.waitKey(0)
   return bbox
+
+if __name__ == '__main__':
+  # setup video capture
+  cap = cv2.VideoCapture("/Users/claraw/Desktop/Feature_Tracking_Optical_Flow/Datasets/Easy/MarquesBrownlee.mp4")
+  ret,img = cap.read()
+  cap.release()
+  if ret:
+    print ("Frame read %s", ret)
+    bbox = detectFace(img)
+  else:
+    print ("Frame read %s", ret)
--- python_function/getFeatures.py
+++ python_function/getFeatures.py
@@ -12,8 +12,45 @@
     - Output x: the x coordinates of features
     - Output y: the y coordinates of features
 '''
+import cv2
+import numpy as np
+from skimage.feature import corner_shi_tomasi, corner_peaks
+from detectFace import detectFace
+import matplotlib.pyplot as plt
 
 def getFeatures(img, bbox):
   #TODO: Your code here
-  
+  img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  img_gray = np.array(img_gray)
+  x = np.array([])
+  y = np.array([])
+
+  for box in bbox:
+    #box = [(x,y),(x+w,y),(x,y+h),(x+w,y+h)]
+    box_img = img_gray[box[0,1]:box[2,1]+1, box[0,0]:box[1,0]+1]
+    xys = corner_peaks(corner_shi_tomasi(box_img, sigma=1))
+    #plt.figure()
+    #plt.imshow(box_img, cmap='gray')
+    #plt.axis('off')
+    #plt.show()
+    y = np.concatenate((y, box[0,1]+ xys[0:len(xys), 0]))
+    x = np.concatenate((x, box[0,0]+ xys[0:len(xys), 1]))
+  plt.figure()
+  plt.imshow(img_gray, cmap='gray')
+  plt.plot(x, y, 'r.')
+  plt.axis('off')
+  plt.show()
   return x, y
+
+if __name__ == '__main__':
+  # setup video capture
+  cap = cv2.VideoCapture("/Users/claraw/Desktop/Feature_Tracking_Optical_Flow/Datasets/Easy/TheMartian.mp4")
+  ret,img = cap.read()
+  cap.release()
+  if ret:
+    print ("Frame read %s", ret)
+    bbox = detectFace(img)
+  else:
+    print ("Frame read %s", ret)
+
+  x, y = getFeatures(img, bbox)
