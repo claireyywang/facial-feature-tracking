7032a83b99a827e6ff572331f7bd233b97a28369
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-11-16 14:52:01 -0500

    clear all

--- python_function/optical_flow_functions.py
+++
@@ -1,8 +0,0 @@
-'''
-  File clarification:
-  Include any helper function you want for this project such as the
-  video frame extraction, video generation, drawing bounding box and so on.
-'''
-
-from detectFace import detectFace
-from getFeatures import getFeatures
