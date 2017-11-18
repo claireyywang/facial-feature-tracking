0433a25ef0b3a5293840b13bda341dd741ca236d
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-11-18 16:46:20 -0500

    add draw boxes function

--- Python_function/helper.py
+++ Python_function/helper.py
@@ -12,13 +12,15 @@
 '''
 
 
-'''
-	input filename can be display on title
-	
-
-'''
 
 from basic_packages import *
+
+'''
+debug_draw:
+	For debug and ouput point and image
+	input filename can be display on title
+'''
+
 
 def debug_draw(img,X,Y,filename=None):
 
@@ -31,3 +33,28 @@ def debug_draw(img,X,Y,filename=None):
 	plt.scatter(x=Y, y=X, c='r', s=3)
 	plt.axis('off')
 	plt.show()
+
+
+
+'''
+draw_box:
+	Draw every box on the image
+'''
+def draw_boxes(img,bbox):
+	img_output = copy.copy(img)
+	for box in bbox:
+		l_t_pt = (box[0])[::-1]
+		r_b_pt = box[3][::-1]
+		cv2.rectangle(img_output,tuple(l_t_pt),tuple(r_b_pt),(0,255,0),2)
+	
+	# # Showing the oupout
+	# cv2.imshow("Faces found" ,img_output)
+	
+	return img_output
+
+
+
+
+
+
+
