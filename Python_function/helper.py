17aa6531327789cbdfadb17f1e7b5b5e2334ef1a
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-11-25 00:06:39 -0500

    add Gaussian functions

--- Python_function/helper.py
+++ Python_function/helper.py
@@ -12,7 +12,6 @@
 '''
 
 
-
 from basic_packages import *
 
 '''
@@ -22,7 +21,7 @@ debug_draw:
 '''
 
 
-def debug_draw(img,X,Y,filename=None):
+def debug_draw(img,X=None,Y=None,filename=None):
 
 	plt.figure()
 	img = np.uint8(img)
@@ -30,7 +29,8 @@ def debug_draw(img,X,Y,filename=None):
 		plt.title("{}".format(filename))	
 	plt.imshow(img, cmap='gray')
 	# plt.title("{}".format(filename))	
-	plt.scatter(x=Y, y=X, c='r', s=3)
+	if(X is not None and Y is not None):
+		plt.scatter(x=Y, y=X, c='r', s=3)
 	plt.axis('off')
 	plt.show()
 
@@ -53,7 +53,38 @@ def draw_boxes(img,bbox):
 	return img_output
 
 
+'''
+  Generate two dimensional Gaussian distribution
+  - input mu: the mean of pdf
+  - input sigma: the standard derivation of pdf
+  - input row: length in row axis
+  - input column: length in column axis
+  - output: a 2D matrix represents two dimensional Gaussian distribution
+'''
 
+
+def GaussianPDF_1D(mu, sigma, length):
+  # create an array
+  half_len = length / 2
+
+  if np.remainder(length, 2) == 0:
+    ax = np.arange(-half_len, half_len, 1)
+  else:
+    ax = np.arange(-half_len, half_len + 1, 1)
+
+  ax = ax.reshape([-1, ax.size])
+  denominator = sigma * np.sqrt(2 * np.pi)
+  nominator = np.exp( -np.square(ax - mu) / (2 * sigma * sigma) )
+
+  return nominator / denominator
+
+def GaussianPDF_2D(mu, sigma, row, col):
+  # create row vector as 1D Gaussian pdf
+  g_row = GaussianPDF_1D(mu, sigma, row)
+  # create column vector as 1D Gaussian pdf
+  g_col = GaussianPDF_1D(mu, sigma, col).transpose()
+
+  return signal.convolve2d(g_row, g_col, 'full')
 
 
 
